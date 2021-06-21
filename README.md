3b393977b1aa108325d9f871fbc678866e5b2925
Dimon0476 <dmitry.fodorov@gmail.com>
2021-06-21 21:35:05 +0300

    Step-7

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 [![Actions Status](https://github.com/Dimon0476/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/Dimon0476/frontend-project-lvl1/actions)
 <a href="https://codeclimate.com/github/codeclimate/codeclimate/maintainability"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability" /></a>
 https://asciinema.org/a/gEJ1E32qTX1RbxwZY6IiNXkaq
- https://asciinema.org/a/XumlAk0eoscFdY7QAmcrOCOFP
+https://asciinema.org/a/XumlAk0eoscFdY7QAmcrOCOFP
+https://asciinema.org/a/aKCmXqsErGf4I0JXt2vdkMyzK
  
--- src/index.js
+++ src/index.js
@@ -41,3 +41,37 @@ export function brainCalc() {
         }
     }
 }
+
+export function brainGcd() {
+    console.log('Find the greatest common divisor of given numbers.'); 
+    for (let i = 1; i <= 3; i += 1) {
+        let num1 = Math.floor(Math.random() * 10);
+        let num2 = Math.floor(Math.random() * 10);
+        //var randOper = Math.floor(Math.random() * oper.length);
+        console.log(`Question: ${num1} ${num2}`);
+        let answer = Number(readlineSync.question("Your answer: "));
+    
+        let a = [num1, num2];
+        var n = a.length, x = Math.abs(a[0]);
+        for (var j = 1; j < n; j += 1) { 
+            var y = Math.abs(a[j]);
+            while (x && y) { 
+                x > y ? x %= y : y %= x;
+            } 
+        }
+        x += y;   
+     // x - просчитан правильный ответ НОК.
+     
+    if (x === answer) {
+        console.log('Correct!');
+    }
+    if (x !== answer) {
+        console.log(`'${answer}' is wrong answer. Correct answer was '${x}'`);
+        console.log(`Let's try again, ${userName}!`);
+        i += 3;
+    }
+    if (i === 3) {
+        console.log(`Congratulations, ${userName}!`)
+    }
+    }     
+}
