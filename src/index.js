2c85304d5ddff7f3241676f8956ffd89b31c942b
Dimon0476 <dmitry.fodorov@gmail.com>
2021-06-30 18:35:17 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const startGame = ({ description, getData }) => {
     }
     console.log('Correct!');
   }
-  console.log(`Congratulations, ${username}`);
+  console.log(`Congratulations, ${username}!`);
 };
 
 export default startGame;
